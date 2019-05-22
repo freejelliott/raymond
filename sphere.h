080ad80c4c44aa8ecaa5c31af32049d412885dba
Joshua <Joshua Elliott>
2019-05-22 15:32:20 -0700

    Format everything. Add diffuse lighting.

--- sphere.h
+++ sphere.h
@@ -2,38 +2,40 @@
 #define SPHEREH
 #include "hitable.h"
 
-class sphere: public hitable {
-public:
-    sphere() {}
-    sphere(vec3 cen, float r) : center(cen), radius(r) {}
-    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
-    vec3 center;
-    float radius;
+class sphere : public hitable {
+ public:
+  sphere() {}
+  sphere(vec3 cen, float r) : center(cen), radius(r) {}
+  virtual bool hit(const ray& r, float t_min, float t_max,
+                   hit_record& rec) const;
+  vec3 center;
+  float radius;
 };
 
-bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = dot(r.direction(), oc);
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - a*c;
-    if (discriminant > 0) {
-        float temp = (-b - sqrt(discriminant)) / a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            return true;
-        }
-        temp = (-b + sqrt(discriminant)) / a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.point_at_parameter(rec.t);
-            rec.normal = (rec.p - center) / radius;
-            return true;
-        }
+bool sphere::hit(const ray& r, float t_min, float t_max,
+                 hit_record& rec) const {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = dot(r.direction(), oc);
+  float c = dot(oc, oc) - radius * radius;
+  float discriminant = b * b - a * c;
+  if (discriminant > 0) {
+    float temp = (-b - sqrt(discriminant)) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      return true;
     }
-    return false;
+    temp = (-b + sqrt(discriminant)) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.point_at_parameter(rec.t);
+      rec.normal = (rec.p - center) / radius;
+      return true;
+    }
+  }
+  return false;
 }
 
 #endif
